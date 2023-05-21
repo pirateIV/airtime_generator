a7d4e640572fe5554bbbf9a5038b642c9975470f
Benjamin Oluwadarasimi <benabolade@gmail.com>
2023-05-21 21:23:58 +0100

    added a js file

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,17 +1,3 @@
-const networkOption = document.getElementById('network');
-const amount = document.getElementById('amount');
-const generateBtn = document.getElementById('generate')
-const saveBtn = document.getElementById('saveBtn')
-const genInput = document.getElementById('genePin')
-
-
-let pin;
-let selected = true;
-let recentDate = new Date() 
-
-let selPin = document.getElementById('selPin')
-let networkProv = document.getElementById('networkProv')
-const savedPin = document.getElementById('savePin')
 
 function loadEvents(){
   generateBtn.addEventListener('click', generatePin)
@@ -66,6 +52,7 @@ function checkSelection(){
     // Enable the save button
     saveBtn.style.display = 'inline-block'
     generateBtn.addEventListener('click', ()=>{
+      error.classList.add('')
       genInput.classList.add('green')
     })
 
---
+++ js/variables.js
@@ -0,0 +1,14 @@
+const networkOption = document.getElementById('network');
+const amount = document.getElementById('amount');
+const generateBtn = document.getElementById('generate')
+const saveBtn = document.getElementById('saveBtn')
+const genInput = document.getElementById('genePin')
+
+
+let pin;
+let selected = true;
+let recentDate = new Date() 
+
+let selPin = document.getElementById('selPin')
+let networkProv = document.getElementById('networkProv')
+const savedPin = document.getElementById('savePin')
